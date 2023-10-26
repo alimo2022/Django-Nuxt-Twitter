91bd500a17e7aeff8a05d21d9b3c52d7e6316c38
alimo2022 <moghimi9877@gmail.com>
2023-10-26 23:10:12 +0330

    disable docker (for now)

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -48,18 +48,18 @@ DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TEMPLATE_CONTEXT": True,
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
-if env("USE_DOCKER") == "yes":
-    import socket
+# INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+# if env("USE_DOCKER") == "yes":
+#     import socket
 
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
-    try:
-        _, _, ips = socket.gethostbyname_ex("node")
-        INTERNAL_IPS.extend(ips)
-    except socket.gaierror:
-        # The node container isn't started (yet?)
-        pass
+#     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+#     INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+#     try:
+#         _, _, ips = socket.gethostbyname_ex("node")
+#         INTERNAL_IPS.extend(ips)
+#     except socket.gaierror:
+#         # The node container isn't started (yet?)
+#         pass
 
 # django-extensions
 # ------------------------------------------------------------------------------
